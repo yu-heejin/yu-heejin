0d605e3b115d3a94d83cbf3f925183028fb7da7d
yu-heejin <yuheejin0914@gmail.com>
2024-03-30 08:19:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,32 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-## Enjoys solving problems and controlling flow with JavaScript
-[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fbitnaleeeee&count_bg=%23555555&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=vo3ov1111)](https://solved.ac/vo3ov1111/)
+
+## Project
+### Devridge (2023.12.28 ~)
+> Community for Developers
+>
+**Repository**
+* [Backend](https://github.com/devridge-team-project/devridge-server)
+### GGOOMi (2023.08.21 ~ 2023.09.02)
+> Draw my diary with DALLE ☺️
+>
+**Repository**
+* [Backend](https://github.com/prompter-day-2023)
+### Ladder (2022.08.27 ~ 2022.10.01)
+> Website Project changing recognized person in picture
+>
+**Repository**
+* [DevOps](https://github.com/2022-SeongNam-Team-C/Ladder-docker)
+* [Backend](https://github.com/2022-SeongNam-Team-C/Ladder-Backend)
+### Police In My Pocket (2022.04.15 ~ 2022.11.30)
+> App Project for my safety in Emergency
+>
+**Repository**
+* [Frontend](https://github.com/hanium-project/Police-in-my-pocket-frontend)
+* [Backend](https://github.com/hanium-project/Police-in-my-pocket-backend)
+
 ## Recent Post 
 """  # list of blog posts will be appended here
 
